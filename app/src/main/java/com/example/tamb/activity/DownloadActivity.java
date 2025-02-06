e638f41529e0971ecc4d6d7f34d0af077447fd7d
Soyl <Soylgr@yandex.ru>
2025-02-07 00:07:05 +0300

    Beta

--- app/src/main/java/com/example/tamb/activity/DownloadActivity.java
+++ app/src/main/java/com/example/tamb/activity/DownloadActivity.java
@@ -2,22 +2,21 @@ package com.example.tamb.activity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.tamb.R;
 import com.example.tamb.model.Photo;
 import com.example.tamb.model.SavedPhoto;
@@ -38,6 +37,7 @@ public class DownloadActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         description = findViewById(R.id.descriptionText);
         name = findViewById(R.id.nameViev);
+        imageView.setImageDrawable(null);
         Button selectImageB = findViewById(R.id.selectImageButton);
 
         imagePickerLauncher = registerForActivityResult(
@@ -45,41 +45,48 @@ public class DownloadActivity extends AppCompatActivity {
                 result -> {
                     if (result.getResultCode() == RESULT_OK && result.getData() != null) {
                         Uri selectedImageUri = result.getData().getData();
-                        try {
-                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
-                            imageView.setImageBitmap(bitmap);
-                        } catch (Exception e) {
-                            Toast.makeText(this, "Ошибка загрузки изображения", Toast.LENGTH_SHORT).show();
-                        }
+                        loadImage(selectedImageUri);
                     }
                 }
         );
+
         selectImageB.setOnClickListener(view -> OpenGallery());
+    }
+
+    private void loadImage(Uri imageUri) {
+        Glide.with(this)
+                .load(imageUri)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .error(R.drawable.error_image)
+                .into(imageView);
     }
 
     public void downloadOnClick(View view) {
         savedPhoto();
-        Snackbar snackbar =  Snackbar.make(view,
-                "Отправленно",
-                Snackbar.LENGTH_LONG);
+        Snackbar snackbar = Snackbar.make(view, "Отправленно", Snackbar.LENGTH_LONG);
         snackbar.setBackgroundTint(0XFF555553);
         snackbar.setActionTextColor(0XFF81C784);
         snackbar.show();
     }
 
     private void OpenGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         imagePickerLauncher.launch(intent);
     }
 
     private void savedPhoto() {
         try {
-            Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+            imageView.setDrawingCacheEnabled(true);
+            imageView.buildDrawingCache();
+            Bitmap bitmap = Bitmap.createBitmap(imageView.getDrawingCache());
+            imageView.setDrawingCacheEnabled(false);
+
             Photo photo = new Photo(bitmap, name.getText().toString(), description.getText().toString());
             SavedPhoto.setPhoto(photo);
-        }catch (Exception e) {
+        } catch (Exception e) {
             Log.e("SavedPhoto", "Ошибка при Image -> bitmap", e);
         }
-
     }
+
 }
--- app/src/main/java/com/example/tamb/activity/MainActivity.java
+++ app/src/main/java/com/example/tamb/activity/MainActivity.java
@@ -4,19 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.tamb.R;
 import com.example.tamb.adapter.PhotoAdapter;
-import com.example.tamb.model.Photo;
 import com.example.tamb.model.SavedPhoto;
 
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-    private List<Photo> photos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +25,10 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void onCLicked(View view) {
+    public void onClicked(View view) {
         ViewPager2 viewPager = findViewById(R.id.viewPager);
         PhotoAdapter adapter = new PhotoAdapter(SavedPhoto.getPhotosByPage(0,20));
+        viewPager.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
         viewPager.setAdapter(adapter);
-
-        viewPager.setPageTransformer(new ViewPager2.PageTransformer() {
-            @Override
-            public void transformPage(@NonNull View page, float position) {
-                page.setTranslationX(-position * page.getWidth());
-                page.setAlpha(1 - Math.abs(position));
-            }
-        });
     }
 }
