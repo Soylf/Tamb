6cb106419bdebbadc271715e94f09ee0c35ebcb7
Soyl <Soylgr@yandex.ru>
2025-03-02 23:19:13 +0300

    Дороботка

--- app/src/main/java/com/example/tamb/model/Constants.java
+++ app/src/main/java/com/example/tamb/model/Constants.java
@@ -3,9 +3,9 @@ package com.example.tamb.model;
 public enum Constants {
     URL_CHAT("https://gigachat.devices.sberbank.ru/api/v1/chat/completions"),
     URL_TOKEN("https://ngw.devices.sberbank.ru:9443/api/v2/oauth"),
-    RqUID("5acd5a3a-41b1-4428-989d-e7d49f8ea592"),
+    RqUID("f464c28a-d642-487e-a489-0cc62a2acc52"),
     MODEL("GigaChat"),
-    AUTHORIZATION("ZTBjNGJkMTYtMjgzZS00MDdkLWI1MmMtZDRiZGEzZDY0OWNhOjVmMDBlYzBkLTlhYmUtNDMyNi04MDUzLTQ5NDVhNDc0OGI2YQ=="),
+    AUTHORIZATION(""),
     ACCESS_TOKEN("");
     private String value;
 
